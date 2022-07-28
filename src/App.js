406ff54a9ccfe1c0353da134b8dec53aa119b084
Jefferson Ortiz <jortizr@ut.edu.co>
2022-07-28 18:13:36 -0500

    agregandos estilos al componente testimonios

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Testimonio from './componentes/Testimonio';
 function App() {
   return (
     <div className="App">
+      <h1>Here is what our alumni say about freeCodeCamp:</h1>
       <Testimonio/>
     </div>
   );
--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../css/Testimonio.css';
 
 function Testimonio(){
   return(
@@ -8,8 +9,8 @@ function Testimonio(){
       src={require('../imagenes/Testimonio-Emma.png')}
       alt='Foto de Emma'/>
       <div className='conten-txt-testimonio'>
-        <p className='cont-txt-testim'>Emma Bostian in Sweden</p>
-        <p className='name-testim'>Software Engineer at Spotify</p>
+        <p className='name-testim'>Emma Bostian in Sweden</p>
+        <p className='cargo-testim'>Software Engineer at Spotify</p>
         <p className='txt-testim'>"I've always struggled with learning JavaScript.
           I've taken many courses but freeCodeCamp's course was
            the one which stuck. Studying JavaScript as well as
