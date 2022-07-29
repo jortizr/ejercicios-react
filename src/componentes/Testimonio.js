ac7ef33d30d613a19080ba3010d7ab3eb1ddfff5
Jefferson Ortiz R <jortizr@ut.edu.cogit config --global user.email jortizr@ut.edu.co>
2022-07-29 03:13:44 -0500

    uso de props en testimonios y arg visual en apps

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -1,22 +1,19 @@
 import React from 'react';
 import '../css/Testimonio.css';
 
-function Testimonio(){
+function Testimonio(props){
   return(
     <div className='contenedor-testimonio'>
-      <img 
+      <img
       className='img-testimonio'
-      src={require('../imagenes/Testimonio-Emma.png')}
+      src={require(`../imagenes/Testimonio-${props.imagen}.png`)}
       alt='Foto de Emma'/>
       <div className='conten-txt-testimonio'>
-        <p className='name-testim'>Emma Bostian in Sweden</p>
-        <p className='cargo-testim'>Software Engineer at Spotify</p>
-        <p className='txt-testim'>"I've always struggled with learning JavaScript.
-          I've taken many courses but freeCodeCamp's course was
-           the one which stuck. Studying JavaScript as well as
-            data structures and algorithms on freeCodeCamp gave
-             me the skills and confidence I needed to land my
-              dream job as a software engineer at Spotify."</p>
+        <p className='name-testim'>
+          <strong>{props.nombre}</strong> in {props.pais}</p>
+        <p className='cargo-testim'>
+        {props.cargo} at <strong>{props.empresa}</strong></p>
+        <p className='txt-testim'>"{props.testimonio}"</p>
       </div>
     </div>
 
