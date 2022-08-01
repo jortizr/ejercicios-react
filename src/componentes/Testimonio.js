0296c4b6c54965747a5ee4e8dca60b0352fc01c5
Jefferson Ortiz <jortizr@ut.edu.co>
2022-08-01 03:47:41 -0500

    correcion texto negrilla invermedio y menu

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import '../css/Testimonio.css';
+// function keysMatriz(matriz, rmatriz){
+//   const listItems = matriz.map((rmatriz)=>
+//   <p key={rmatriz.toString()}>
+//     {rmatriz}
+//   </p>
+// );
+// }
 
 function Testimonio(props){
+  
   return(
+    
     <div className='contenedor-testimonio'>
-      <img
-      className='img-testimonio'
-      src={require(`../imagenes/Testimonio-${props.imagen}.png`)}
-      alt='Foto de Emma'/>
-      <div className='conten-txt-testimonio'>
-        <p className='name-testim'>
-          <strong>{props.nombre}</strong> in {props.pais}</p>
-        <p className='cargo-testim'>
-        {props.cargo} at <strong>{props.empresa}</strong></p>
-        <p className='txt-testim'>"{props.testimonio}"</p>
-      </div>
+        <img
+          className='img-testimonio'
+          src={require(`../imagenes/Testimonio-${props.imagen}.png`)}
+          alt={`esta es la foto de ${props.nombre}`} />
+        <div className='conten-txt-testimonio'>
+          <p className='name-testim'>
+            <strong>{props.nombre}</strong> in {props.pais}</p>
+          <p className='cargo-testim'>
+            {props.cargo} at <strong>{props.empresa}</strong></p>
+          <p>"{props.testimonio}"</p>
+        </div>
     </div>
 
   );
